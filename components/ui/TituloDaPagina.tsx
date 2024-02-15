83ca7a3703db48d91bb3e36bbb97bd3c2a62b485
Ana <ana@agenciafg.com.br>
2024-02-15 12:03:12 -0300

    ajustes

--- components/ui/TituloDaPagina.tsx
+++ components/ui/TituloDaPagina.tsx
@@ -6,7 +6,7 @@ export interface Props {
   export default function TituloDaPagina({ titulo, subtitulo }: Props) {
     return (
       <div>
-        <h1 class="font-semibold text-center mt-8 text-4xl">{titulo}</h1>
+        <h1 class="font-semibold text-center mt-8 text-4xl">{titulo}</h1>  
         <h2 class="font-medium text-center mt-4 mb-8">{subtitulo}</h2>
       </div>
     );
